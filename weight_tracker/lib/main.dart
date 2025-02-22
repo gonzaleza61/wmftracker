98c930bb16ec64101c1aaad33afd5e50cf76982e
gonzaleza61 <gonzaleza61@gator.uhd.edu>
2025-02-22 17:12:50 -0600

    remove debug banner

--- weight_tracker/lib/main.dart
+++ weight_tracker/lib/main.dart
@@ -38,6 +38,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'WMF',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.red),
       home: AuthWrapper(),
     );
